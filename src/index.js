822729c278a1fb8026dbebdfee88ac79c4efc2f3
FaiiFaii09 <67331358+FaiiFaii09@users.noreply.github.com>
2020-08-29 19:50:13 -0700

    Reformatted search engine in js file

--- src/index.js
+++ src/index.js
@@ -48,10 +48,10 @@ function formatHours(timestamp) {
 }
 
 // Display Temperature & others
-function displayTemperature(response) {
+function displayCurrentWeather(response) {
   console.log(response.data);
   let location = response.data.name;
-  let locationName = document.querySelector("h2");
+  let locationName = document.querySelector("#city");
   locationName.innerHTML = `${location}`;
 
   let description = response.data.weather[0].description;
@@ -98,7 +98,7 @@ function displayTemperature(response) {
 
   celsiusTemp = response.data.main.temp;
 }
-// Current Position
+// Search User's Current Position
 function myLocation(position) {
   console.log(position);
   let currentLat = position.coords.latitude;
@@ -106,7 +106,7 @@ function myLocation(position) {
   let apiKey = "751f58f905d4f428f0ffbf70ca07ab69";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayCurrentWeather);
 }
 
 function getCurrentPosition(event) {
@@ -122,15 +122,21 @@ currentLocationButton.addEventListener("click", getCurrentPosition);
 function searchLocation(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-location");
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${searchInput.value}`;
-  let apiKey = "751f58f905d4f428f0ffbf70ca07ab69";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
+  let locationName = document.querySelector("#city");
+  locationName.innerHTML = `${searchInput.value}`;
+  locationSearch(searchInput.value);
 }
+function locationSearch(city) {
+  let apiKey = "751f58f905d4f428f0ffbf70ca07ab69";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-function displayFahrenheitTemp(event) {
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchLocation);
+
+// Unit Converter
+function convertToFahrenheitTemp(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature-value");
   celsiusLink.classList.remove("active");
@@ -139,7 +145,7 @@ function displayFahrenheitTemp(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
-function displayCelsiusTemp(event) {
+function convertToCelsiusTemp(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
@@ -149,11 +155,10 @@ function displayCelsiusTemp(event) {
 
 let celsiusTemp = null;
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchLocation);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+fahrenheitLink.addEventListener("click", convertToFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemp);
+celsiusLink.addEventListener("click", convertToCelsiusTemp);
+
+locationSearch("Solvang, USA");
